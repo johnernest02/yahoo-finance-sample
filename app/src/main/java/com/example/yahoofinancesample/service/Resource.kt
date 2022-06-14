2f19c109ddb6e14b6b245809414a756aca4022dd
John Ernest Guadalupe <john.ernest@toptal.com>
2022-06-14 10:06:05 +0800

    Add new test for 8 second verification

--- app/src/main/java/com/example/yahoofinancesample/service/Resource.kt
+++ app/src/main/java/com/example/yahoofinancesample/service/Resource.kt
@@ -1,7 +1,7 @@
 package com.example.yahoofinancesample.service
 
 sealed class Resource<out T> {
-    class Loading<out T> : Resource<T>()
+    class Loading<Nothing> : Resource<Nothing>()
     data class Success<out T>(val data: T) : Resource<T>()
     data class Failure<out T>(val throwable: Throwable) : Resource<T>()
 }
--- app/src/test/java/com/example/yahoofinancesample/MarketDataListViewModelTest.kt
+++ app/src/test/java/com/example/yahoofinancesample/MarketDataListViewModelTest.kt
@@ -14,10 +14,16 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.verify
+import org.mockito.AdditionalAnswers
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.stubbing.Answer
+import retrofit2.Invocation
 import retrofit2.Response
 import com.example.yahoofinancesample.service.responsemodels.Result as MarketData
 
@@ -34,6 +40,56 @@ class MarketDataListViewModelTest {
 
     private val marketDataObserver: Observer<Resource<List<MarketData>>> = mock()
 
+    private val sampleMarketSummary = MarketSummary(
+        marketSummaryAndSparkResponse = MarketSummaryAndSparkResponse(
+            error = null,
+            result = listOf(
+                MarketData(
+                    exchangeTimezoneName = "America/New_York",
+                    fullExchangeName = "CME",
+                    symbol = "ES=F",
+                    gmtOffSetMilliseconds = -14400000,
+                    exchangeDataDelayedBy = 10,
+                    firstTradeDateMilliseconds = 969249600000,
+                    language = "en-US",
+                    exchangeTimezoneShortName = "EDT",
+                    quoteType = "FUTURE",
+                    customPriceAlertConfidence = "NONE",
+                    marketState = "REGULAR",
+                    market = "us24_market",
+                    priceHint = 2,
+                    tradeable = false,
+                    sourceInterval = 10,
+                    exchange = "CME",
+                    shortName = "S&P Futures",
+                    region = "US",
+                    triggerable = false,
+                    regularMarketPreviousClose = RegularMarketPreviousCloseX(
+                        raw = "4016.25",
+                        fmt = "4,016.25",
+                    ),
+                    regularMarketTime = RegularMarketTime(
+                        raw = "1654857400",
+                        fmt = "6:36AM EDT",
+                    ),
+                    spark = Spark(
+                        symbol = "ES=F",
+                        previousClose = 4016.25,
+                        chartPreviousClose = 4016.25,
+                        dataGranularity = "300",
+                        end = "1654919940",
+                        start = "1654833600",
+                        timestamp = listOf(1654833600, 1654833900),
+                        close = listOf(4024.75, 4023.5)
+                    )
+                )
+            )
+        )
+    )
+
+    @Captor
+    private lateinit var captor: ArgumentCaptor<Resource<List<MarketData>>>
+
     @Before
     fun setUp() {
         yahooFinanceAPIService = mock()
@@ -43,59 +99,34 @@ class MarketDataListViewModelTest {
     @Test
     fun `Given loadMarketData is called, When no error occurs, Then getMarketSummary should be invoked successfully`() {
         runTest {
-            var sampleMarketSummary = MarketSummary(
-                marketSummaryAndSparkResponse = MarketSummaryAndSparkResponse(
-                    error = null,
-                    result = listOf(
-                        MarketData(
-                            exchangeTimezoneName = "America/New_York",
-                            fullExchangeName = "CME",
-                            symbol = "ES=F",
-                            gmtOffSetMilliseconds = -14400000,
-                            exchangeDataDelayedBy = 10,
-                            firstTradeDateMilliseconds = 969249600000,
-                            language = "en-US",
-                            exchangeTimezoneShortName = "EDT",
-                            quoteType = "FUTURE",
-                            customPriceAlertConfidence = "NONE",
-                            marketState = "REGULAR",
-                            market = "us24_market",
-                            priceHint = 2,
-                            tradeable = false,
-                            sourceInterval = 10,
-                            exchange = "CME",
-                            shortName = "S&P Futures",
-                            region = "US",
-                            triggerable = false,
-                            regularMarketPreviousClose = RegularMarketPreviousCloseX(
-                                raw = "4016.25",
-                                fmt = "4,016.25",
-                            ),
-                            regularMarketTime = RegularMarketTime(
-                                raw = "1654857400",
-                                fmt = "6:36AM EDT",
-                            ),
-                            spark = Spark(
-                                symbol = "ES=F",
-                                previousClose = 4016.25,
-                                chartPreviousClose = 4016.25,
-                                dataGranularity = "300",
-                                end = "1654919940",
-                                start = "1654833600",
-                                timestamp = listOf(1654833600, 1654833900),
-                                close = listOf(4024.75, 4023.5)
-                            )
-                        )
-                    )
-                )
-            )
+            val mockLoading = Resource.Loading<List<MarketData>>()
             doReturn(Response.success(sampleMarketSummary))
                 .`when`(yahooFinanceAPIService)
                 .getMarketSummary()
             marketDataListViewModel.getMarketData().observeForever(marketDataObserver)
-            TestScope().launch {
+            this.launch {
                 verify(yahooFinanceAPIService).getMarketSummary()
+                verify(marketDataObserver, times(1)).onChanged(captor.capture())
                 verify(marketDataObserver).onChanged(Resource.Success(sampleMarketSummary.marketSummaryAndSparkResponse.result))
+            }
+            marketDataListViewModel.getMarketData().removeObserver(marketDataObserver)
+        }
+    }
+
+    @Test
+    fun `Viewmodel getMarketSummary should post new responses every 8 seconds`() {
+        runTest {
+            doReturn(Response.success(sampleMarketSummary))
+                .`when`(yahooFinanceAPIService)
+                .getMarketSummary()
+
+            marketDataListViewModel.getMarketData().observeForever(marketDataObserver)
+            this.launch {
+                doAnswer(AdditionalAnswers.answersWithDelay<Invocation>(8000) { null })
+                    .`when`(yahooFinanceAPIService)
+                    .getMarketSummary()
+                verify(yahooFinanceAPIService).getMarketSummary()
+                verify(marketDataObserver, times(2)).onChanged(Resource.Success(sampleMarketSummary.marketSummaryAndSparkResponse.result))
             }
             marketDataListViewModel.getMarketData().removeObserver(marketDataObserver)
         }
